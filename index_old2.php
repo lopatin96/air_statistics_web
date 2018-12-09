ccd7c54c834a6c54cc2aaf7e17aaa1547ec4e8ac
lopatin96 <ilopatin96@mail.ru>
2018-12-09 02:08:27 +0100

    add configs.

--- index_old2.php
+++ index_old2.php
@@ -1,25 +1,50 @@
 <?php
-$host,
-		$username,
-		$passwd,
-		$dbname,
-		$port,
-		$socket
 
-$host = "192.168.1.7";
-$username = "phpmyadmin";
-$passwd = "560492q";
-$dbname = "phpmyadmin";
-$port = 3306;
+require 'vendor/autoload.php';
 
-// Create connection
-$conn = new mysqli($host, $username, $passwd, $dbname, $port);
+use Medoo\Medoo;
 
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-echo "Connected successfully";
+// Initialize
+$database = new Medoo([
+    'database_type' => 'mysql',
+    'database_name' => 'phpmyadmin',
+    'server' => '127.0.0.1',
+    'username' => 'phpmyadmin',
+    'password' => '560492q',
+
+
+	// [optional]
+	'charset' => 'utf8mb4',
+	'collation' => 'utf8mb4_general_ci',
+	'port' => 3306,
+
+	// [optional] Table prefix
+	'prefix' => 'PREFIX_',
+
+	// [optional] Enable logging (Logging is disabled by default for better performance)
+	'logging' => true,
+
+	// [optional] MySQL socket (shouldn't be used with server and port)
+	'socket' => '/tmp/mysql.sock',
+
+	// [optional] driver_option for connection, read more from http://www.php.net/manual/en/pdo.setattribute.php
+	'option' => [
+		PDO::ATTR_CASE => PDO::CASE_NATURAL
+	],
+
+	// [optional] Medoo will execute those commands after connected to the database for initialization
+	'command' => [
+		'SET SQL_MODE=ANSI_QUOTES'
+	]
+]);
+
+$data = $database->select('air_statistics', [
+    'datetime',
+    'temperature',
+    'humidity'
+]);
+
+echo json_encode($data);
 
 ?>
 
