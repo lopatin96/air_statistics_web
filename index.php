1ba873af997d204dc02d7de95f24be50a36c0cb8
lopatin96 <ilopatin96@mail.ru>
2018-12-09 20:20:11 +0100

    Some clean up.

--- index.php
+++ index.php
@@ -5,29 +5,11 @@ $passwd = "560492q";
 $dbname = "phpmyadmin";
 $port = 3306;
 
-// Create connection
 $conn = new mysqli($host, $username, $passwd, $dbname, $port);
-
-//// Check connection
-//if ($conn->connect_error) {
-//    die("Connection failed: " . $conn->connect_error);
-//}
-//echo "Connected successfully";
 
 $sql = "SELECT datetime, CONCAT(HOUR(datetime), ':', IF (MINUTE(datetime) < 10, '00', FLOOR(MINUTE(datetime)/10) * 10)) as time, AVG(`temperature`) as temperature, AVG(`humidity`) as humidity, FLOOR((TIMESTAMP(datetime) - TIMESTAMP(DATE(NOW()))) / 1000) as timestamp FROM `air_statistics` WHERE DATE(`datetime`) = DATE(NOW()) GROUP BY timestamp";
 $result = $conn->query($sql);
 $data = $result->fetch_all();
-
-//if ($result->num_rows > 0) {
-//    // output data of each row
-//    while($row = $data) {
-//        echo "id: " . $row["id"]. " - datetime: " . $row["datetime"]. ", t:" . $row["temperature"] . ", h:" . $row["humidity"] . "<br>";
-//    }
-//} else {
-//    echo "0 results";
-//}
-
-//echo var_dump($data);
 
 function arrayToJson($array, $column_id)
 {
