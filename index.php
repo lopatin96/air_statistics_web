e36726281c02746c939fa899a48e32bdcdbad571
lopatin96 <ilopatin96@mail.ru>
2018-12-09 02:02:06 +0100

    add configs.

--- index.php
+++ index.php
@@ -8,10 +8,34 @@ use Medoo\Medoo;
 $database = new Medoo([
     'database_type' => 'mysql',
     'database_name' => 'phpmyadmin',
-    'server' => '192.168.1.7',
+    'server' => '192.168.1.7/phpmyadmin',
     'username' => 'phpmyadmin',
     'password' => '560492q',
-    'port' => 3306,
+
+
+	// [optional]
+	'charset' => 'utf8mb4',
+	'collation' => 'utf8mb4_general_ci',
+	'port' => 3306,
+
+	// [optional] Table prefix
+	'prefix' => 'PREFIX_',
+
+	// [optional] Enable logging (Logging is disabled by default for better performance)
+	'logging' => true,
+
+	// [optional] MySQL socket (shouldn't be used with server and port)
+	'socket' => '/tmp/mysql.sock',
+
+	// [optional] driver_option for connection, read more from http://www.php.net/manual/en/pdo.setattribute.php
+	'option' => [
+		PDO::ATTR_CASE => PDO::CASE_NATURAL
+	],
+
+	// [optional] Medoo will execute those commands after connected to the database for initialization
+	'command' => [
+		'SET SQL_MODE=ANSI_QUOTES'
+	]
 ]);
 
 $data = $database->select('air_statistics', [
